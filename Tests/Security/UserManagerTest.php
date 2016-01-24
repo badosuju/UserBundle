209ed8ff8293dc5a2d52f59379eb7f04b0e57d63
Matt Holbrook-Bull <matt@ampisoft.com>
2016-01-24 19:56:58 +0000

    failed test to force ready status

---
+++ Tests/Security/UserManagerTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Matt Holbrook-Bull <matt@ampisoft.com>
+ * 24/01/16
+ */
+
+namespace AmpUserBundle\Tests\Security;
+
+
+/**
+ * @author Matt Holbrook-Bull <matt@ampisoft.com>
+ *
+ * Class UserManagerTest
+ * @package AmpUserBundle\Tests\Security
+ */
+class UserManagerTest extends \PHPUnit_Framework_TestCase {
+
+    public function testLoadUser() {
+        $fail = true;
+
+        $this->assertFalse($fail);
+    }
+
+
+}

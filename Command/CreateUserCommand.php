975bb1bc819710b46ea975e9cf6f44f41b2b0205
Matt Holbrook-Bull <matt@ampisoft.com>
2016-02-25 13:08:48 +0000

    fix merge messups

---
+++ Command/CreateUserCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AmpUserBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateUserCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('amp:user:create')
+            ->setDescription('Hello PhpStorm');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}

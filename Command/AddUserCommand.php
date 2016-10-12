84d39b9c17ef3ad0520a47226e5ff90685cd78ca
Matt Holbrook-Bull <matt.holbrook-bull@os.uk>
2016-10-12 14:06:52 +0100

    fix user add

--- Command/AddUserCommand.php
+++ Command/AddUserCommand.php
@@ -40,7 +40,7 @@ class AddUserCommand extends ContainerAwareCommand
 
         $userManager = $this->getContainer()->get('amp_user.manager');
 
-        $user = $userManager->createUser($username, $plainPassword, $email, ['ROLE_ADMIN']);
+        $user = $userManager->createUser($username, $plainPassword, $email, 'ROLE_ADMIN');
 
         $output->writeln(sprintf('User: %s created', $user->getUsername()));
 

5838d483c8728593b4f06cad5d3ada8712eb1202
Matt Holbrook-Bull <matt.holbrook-bull@os.uk>
2017-03-03 13:19:37 +0000

    tests

---
+++ Tests/Entity/AbstractUserTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/********************************
+ * Author: M Holbrook-Bull
+ * Date  : 01/03/2017
+ ********************************/
+
+namespace Ampisoft\UserBundle\Tests;
+
+use Ampisoft\UserBundle\Entity\AbstractGroup;
+use Ampisoft\UserBundle\Entity\AbstractUser;
+use PHPUnit\Framework\TestCase;
+
+class AbstractUserTest extends TestCase
+{
+
+
+    public function testSettersAndGetters()
+    {
+
+
+        $user = new TestUser();
+
+        $reflection = new \ReflectionClass(TestUser::class);
+
+        $setters = array_filter($reflection->getMethods(), function($method){
+            return strpos($method, 'set') !== false;
+        });
+
+        /** @var \ReflectionMethod $method */
+        foreach ($setters as $method) {
+            $setName = $method->getName();
+            $getName = str_replace('set', 'get', $method->getName());
+
+            $params = $method->getParameters();
+            if($params[0]->isArray()) {
+                $data = ['test'];
+                $array = true;
+            } else {
+                $data = 'test';
+                $array = false;
+            }
+
+            $user->$setName($data);
+            self::assertEquals($array ? ['test'] : 'test', $user->$getName());
+        }
+
+
+
+    }
+
+    public function testHasMethods()
+    {
+
+        $user = new TestUser();
+
+        $reflection = new \ReflectionClass(TestUser::class);
+
+        $has = array_filter($reflection->getMethods(), function ($method) {
+            return strpos($method, 'has') !== false;
+        });
+
+        foreach ($has as $method) {
+            $data = null;
+
+            switch($method->getName()) {
+                case 'hasRole':
+                    $data = 'ROLE_ADMIN';
+                    continue;
+                    break;
+            }
+            $m = $method->getName();
+            self::assertFalse($user->$m($data));
+        }
+    }
+
+
+}
+
+
+class TestUser extends AbstractUser
+{
+
+}
+
+class TestGroup extends AbstractGroup
+{
+
+}
